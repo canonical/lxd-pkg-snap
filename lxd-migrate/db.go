a616c2f43ab184a851a526b5d85d21e5ea496575
Stéphane Graber <stgraber@ubuntu.com>
2018-05-04 09:33:33 +0000

    Add support for LXD 3.0 source
    This adds the logic needed to update the database of LXD 3.0 using the
    new patch mechanism.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd-migrate/db.go
+++ lxd-migrate/db.go
@@ -2,36 +2,51 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/lxc/lxd/shared"
 )
 
-type dbInstance struct {
-	db *sql.DB
-}
+func dbRewritePoolSource(dst *lxdDaemon, pool string, path string) error {
+	if strings.HasPrefix(dst.info.Environment.ServerVersion, "2.") {
+		// LXD 2.x target
+		db, err := sql.Open("sqlite3", filepath.Join(dst.path, "lxd.db"))
+		if err != nil {
+			return err
+		}
 
-func dbOpen(path string) (*dbInstance, error) {
-	// Open the database
-	db, err := sql.Open("sqlite3", path)
-	if err != nil {
-		return nil, err
+		_, err = db.Exec("UPDATE storage_pools_config SET value=? WHERE key='source' AND storage_pool_id=(SELECT id FROM storage_pools WHERE name=?);", path, pool)
+		if err != nil {
+			return err
+		}
+
+		err = db.Close()
+		if err != nil {
+			return err
+		}
 	}
 
-	// Setup the internal struct
-	d := &dbInstance{db: db}
+	// Recent LXD target
+	if !shared.PathExists(filepath.Join(dst.path, "database")) {
+		err := os.MkdirAll(filepath.Join(dst.path, "database"), 0700)
+		if err != nil {
+			return err
+		}
+	}
 
-	return d, nil
-}
-
-func (d *dbInstance) updateStoragePoolSource(pool string, path string) error {
-	id := int64(-1)
-	statement := `SELECT id FROM storage_pools WHERE name=?;`
-	err := d.db.QueryRow(statement, pool).Scan(&id)
+	// Setup the DB patch
+	patchPath := filepath.Join(dst.path, "database", "patch.global.sql")
+	patch, err := os.OpenFile(patchPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
 		return err
 	}
 
-	_, err = d.db.Exec("UPDATE storage_pools_config SET value=? WHERE key='source' AND storage_pool_id=?;", path, id)
+	_, err = patch.WriteString(fmt.Sprintf("UPDATE storage_pools_config SET value='%s' WHERE key='source' AND storage_pool_id=(SELECT id FROM storage_pools WHERE name='%s');\n", path, pool))
 	if err != nil {
 		return err
 	}
