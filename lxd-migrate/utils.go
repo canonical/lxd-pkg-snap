070bfc01eee2ef3d75e9fe625cd8230b50b4a59d
Stéphane Graber <stgraber@ubuntu.com>
2018-09-17 15:43:22 +0200

    lxd-migrate: Whitelist lxc2
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- lxd-migrate/utils.go
+++ lxd-migrate/utils.go
@@ -159,7 +159,7 @@ func packagesRemovable(names []string) error {
 	for _, pkg := range rdepends {
 		output, err := shared.RunCommand("dpkg-query", "-W", "-f=${Status}", pkg)
 		if err == nil && strings.HasSuffix(output, " installed") {
-			if shared.StringInSlice(pkg, []string{"adapt", "autopkgtest", "nova-compute-lxd", "snapcraft", "ubuntu-server"}) {
+			if shared.StringInSlice(pkg, []string{"adapt", "autopkgtest", "lxc2", "nova-compute-lxd", "snapcraft", "ubuntu-server"}) {
 				continue
 			}
 
